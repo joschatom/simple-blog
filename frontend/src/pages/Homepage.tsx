7f69fd9d76af27684779b35250eb70d06d9577a7
Joscha Egloff (ALINK) <joscha.egloff@autismuslink.ch>
2025-11-20 09:34:45 +0100

    [FRONTEND] Basic Login

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,3 +1,34 @@
+import { useContext } from "react";
+import { Client } from "../client";
+import { User } from "blog-api";
+import { useNavigate } from "react-router";
+import { Button } from "../components/Button";
+
 export function Homepage() {
-  return <>Hello World</>;
+  const client = useContext(Client);
+
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <form
+        action={async (data) => {
+          await client.login(
+            data.get("username")!.toString(),
+            data.get("password")!.toString()
+          );
+          alert(JSON.stringify(await User.currentUser(client), null, 2));
+          navigate("/users/me");
+        }}
+      >
+        <label htmlFor="username">Username: </label>
+        <input id="username" name="username" />
+        <br />
+        <label htmlFor="password">Password: </label>
+        <input id="password" name="password" type="password" />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
 }
