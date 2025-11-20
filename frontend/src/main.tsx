b31856879371d5291481af9001f5800878d63c88
Joscha Egloff (ALINK) <joscha.egloff@autismuslink.ch>
2025-11-20 10:14:01 +0100

    [FRONTEND] Added Material UI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './index.css' 
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -2,6 +2,25 @@ import { useContext } from "react";
 import { Client } from "../client";
 import { User } from "blog-api";
 import { useNavigate } from "react-router";
+<<<<<<< Updated upstream
+=======
+import {
+  Button,
+  Container,
+  createTheme,
+  CssBaseline,
+  FormLabel,
+  Input,
+  OutlinedInput,
+  ThemeProvider,
+} from "@mui/material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark"
+  },
+});
+>>>>>>> Stashed changes
 
 export function Homepage() {
   const client = useContext(Client);
@@ -10,24 +29,44 @@ export function Homepage() {
 
   return (
     <>
-      <form
-        action={async (data) => {
-          await client.login(
-            data.get("username")!.toString(),
-            data.get("password")!.toString()
-          );
-          alert(JSON.stringify(await User.currentUser(client), null, 2));
-          navigate("/users/me");
-        }}
-      >
-        <label htmlFor="username">Username: </label>
-        <input id="username" name="username" />
-        <br />
-        <label htmlFor="password">Password: </label>
-        <input id="password" name="password" type="password" />
-        <br />
-        <button type="submit">Login</button>
-      </form>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Container maxWidth={"lg"}>
+          <form
+            noValidate
+            autoComplete="off"
+            action={async (data) => {
+              console.log(data);
+
+              await client.login(
+                data.get("username")!.toString(),
+                data.get("password")!.toString()
+              );
+              alert(JSON.stringify(await User.currentUser(client), null, 2));
+              navigate("/users/me");
+            }}
+          >
+            <div className="form-field">
+              <FormLabel htmlFor="username">Username</FormLabel>
+              <Input id="username" name="username" />
+            </div>
+
+            <div>
+              <FormLabel htmlFor="password">Password </FormLabel>
+              <Input id="password" name="password" type="password" />
+            </div>
+
+            <br/>
+
+            <div>
+              <Button variant="contained" type="submit">
+              Login
+            </Button>
+            
+            </div>
+          </form>
+        </Container>
+      </ThemeProvider>
     </>
   );
 }
